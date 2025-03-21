1284b8afb7d4d94712b525f92e49eb4f40240805
adityaSharma21111992 <30341595+adityaSharma21111992@users.noreply.github.com>
2025-03-21 08:50:56 +0000

    Data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 23453
+let accountEmail = "test@test.com"
+var accountPassword = "aditya"
+accountCity = "Indore"
+let accountState;
+
+/*
+do not use var in java script 
+use only let as var has issue in finction scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/02_Data_Types.js
@@ -0,0 +1,17 @@
+"Use Strict"; //this text is useed to use all JS Code as newer version 
+
+let Name ="Aditya"
+let age = 32
+let state;
+//types of data type in java script
+// number = 2 to power of 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// Null
+// undefined 
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
