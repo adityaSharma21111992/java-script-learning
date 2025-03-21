73315e02e0f8373da64a2e2e1916789312da16b7
adityaSharma21111992 <30341595+adityaSharma21111992@users.noreply.github.com>
2025-03-21 09:38:15 +0000

    comparsion

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log(2>1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// === this operator check strictly and compares the data type, value everthing
+
+console.log(2 === 2)
