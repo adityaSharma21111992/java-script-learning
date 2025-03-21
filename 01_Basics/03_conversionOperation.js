d804a968780f29597b59e40e741d66a1c145d7a0
adityaSharma21111992 <30341595+adityaSharma21111992@users.noreply.github.com>
2025-03-21 09:01:54 +0000

    Conversion Operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+console.log(typeof score); //output String 
+
+//conversiont of number to string 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //output Number
+console.log(valueInNumber); //output NaN(not a number)
+
+
+
+
